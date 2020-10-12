3338fff359b3e0e14c08ca518b379d16126ff6aa
Muhammad junaid khan <mohammadjunaidirfan@gmail.com>
2020-10-13 01:23:59 +0500

    add file

---
+++ family.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+#include<conio.h>
+using namespace std;
+
+class parent{
+	public:
+		char fathername[15];
+		char childname[15];
+		int dateofbirth; const
+		int day; const
+		int month; const
+		int year; const
+		int code; const
+		int age;
+		
+		void getdata(){
+			cout<<"\n\tFeed Information...";
+			
+		}
+		
+};
+
+
+
+int main(){
+	
+}
