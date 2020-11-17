03f0ac0747dc99260247b15ef0a52a136c1e273e
Muhammad junaid khan <mohammadjunaidirfan@gmail.com>
2020-11-17 14:10:58 +0500

    add create family function

--- family.cpp
+++ family.cpp
@@ -7,17 +7,51 @@ using namespace std;
 
 class parent{
 	public:
-		char fathername[15];
-		char childname[15];
-		int dateofbirth; const
-		int day; const
-		int month; const
-		int year; const
-		int code; const
-		int age;
+		string parent_name;
+		string father_name;
+		string mother_name;
+		string husband_name;
+		string wife_name;
+		string alive_or_dead;
+		string employee;
+		string discribtion;
+		string child_name;
+		string boy_name;
+		string daughter_name;
+		string status;
+		string education;
+
+	
+		void mainmenu(){
+			cout<<"========================welcome======================="<<endl<<endl;
+			cout<<"\t1.create family."<<endl;
+			cout<<"\t2.search family."<<endl;
+			
+		}
 		
-		void getdata(){
-			cout<<"\n\tFeed Information...";
+		void data_to_create_family(){
+			parent child;
+			cout<<"\nenter husband name = ";
+			getline(cin,child.husband_name);
+			cout<<"\nenter wife name = ";
+			getline(cin,child.wife_name);
+			cout<<"\nenter paren name = ";
+			cout<<"\n\tenter father name = ";
+			getline(cin,child.father_name);
+			cout<<"\n\tenter mother name = ";
+			getline(cin,child.mother_name);
+			
+			
+		}
+		void creatfamily(){
+			parent child;
+			fstream f;
+			f.open("family_timeline" , ios::binary);
+			cout<<"\nFeed Information...";
+			child.data_to_create_family();
+			f.write((char*)&child , sizeof(child));
+			f.close();
+			
 			
 		}
 		
@@ -25,6 +59,20 @@ class parent{
 
 
 
+
 int main(){
+	parent parill;
+
+	parill.mainmenu();
+	int ch;
+	cout<<"\nwhat would you like ???...";
+	cin>>ch;
+	cin.ignore();
+	if(ch==1){
+		parill.creatfamily();
+	}
+	
+	
+	
 	
 }
